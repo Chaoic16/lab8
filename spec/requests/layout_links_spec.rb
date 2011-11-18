d73b6a5b36ba3edb50026a79be1f4ab368025dea
David Salazar <Eagle114th@gmail.com>
2011-11-17 18:27:48 -0600

    Successfully set up my own website with working links for News, Contact us, and About us

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   
-    it "should have a home page at '/'" do
+    it "should have a default page at '/'" do
       get '/'
-      response.should have_selector("title", :content => "Home")
+      response.should have_selector("title", :content => "Default")
     end
   
      it "should have a contact page at '/contact'" do
@@ -22,10 +22,10 @@ describe "LayoutLinks" do
       response.should have_selector("title", :content => "Help")
     end
     
-#     it "should have a sign-up page at '/signup'" do
-#      get '/signup'
-#      response.should have_selector("title", :content => "Sign up")
-#    end
+# it "should have a sign-up page at '/signup'" do
+# get '/signup'
+# response.should have_selector("title", :content => "Sign up")
+# end
     
     it "should have the right links on the layout" do
       visit root_path
@@ -37,7 +37,7 @@ describe "LayoutLinks" do
       click_link "Home"
       response.should have_selector('title', :content => "Home")
       click_link "Sign up now"
- #     response.should have_selector('title', :content => "Sign up")
+ # response.should have_selector('title', :content => "Sign up")
       response.should have_selector('a[href="/"]>img')
     end
 end
