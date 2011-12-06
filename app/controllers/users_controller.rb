917ca8a5ac6696a42d761abc1bae9ab8d0ce4220
David Salazar <Eagle114th@gmail.com>
2011-12-06 14:44:48 -0600

    Fixed new and show page for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  def new # responds to get /users/new by rendering new.html.erb
+    @title = "Registry"
+    @user = User.new
   end
   
-  def new
-    @title = "Registry"
+  def show # responds to get /user/i by rendering show.html.erb
+    @user = User.find(params[:id])
+    @title = @user.name
   end
 end
